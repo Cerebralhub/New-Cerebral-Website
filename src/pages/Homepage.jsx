0d5ef03341ed7b6d855bcc3825a538ea00fc5fec
Jaachi Okafor <okaforjaachi@gmail.com>
2024-03-24 01:31:59 +0100

    fixed z index issues

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -20,10 +20,10 @@ const Homepage = () => {
                 </p>
                 {/* buttons */}
                 <div className="mt-12 flex space-x-9">
-                  <button className="bg-[#DC141C] text-white font-medium px-7 py-2 rounded-full">
+                  <button className="bg-[#DC141C] text-white font-medium px-7 py-3 rounded-full">
                     What We Do
                   </button>
-                  <button className="bg-[#F58A07] text-white font-medium px-7 py-2 rounded-full">
+                  <button className="bg-[#F58A07] text-white font-medium px-7 py-3 rounded-full">
                     How We Do
                   </button>
                 </div>
@@ -32,15 +32,15 @@ const Homepage = () => {
             </div>
             {/* hero image */}
             <div className="w-2/3 relative">
-              <div className="z-10 bg-[#ED9B18] opacity-50 text-[#ED9B18] w-40 h-[21.125rem] rounded-t-3xl absolute bottom-0 left-20">
+              <div className="z-40 bg-[#ED9B18] opacity-50 text-[#ED9B18] w-40 h-[21.125rem] rounded-t-3xl absolute bottom-0 left-20">
                 .
               </div>
 
-              <div className="z-0 bg-[#F58A07] text-[#F58A07] w-40 h-60 rounded-tr-3xl absolute bottom-0 right-0">
+              <div className="z-0 bg-[#F58A07] opacity-75 text-[#F58A07] w-40 h-60 rounded-tr-3xl absolute bottom-0 right-0">
                 .
               </div>
 
-              <div className="z-30">
+              <div className="z-30 relative">
                 <img src={hereoImage} alt="A man smiling" />
               </div>
             </div>
